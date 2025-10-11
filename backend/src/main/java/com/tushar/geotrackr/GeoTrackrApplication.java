684df9424236d0384734ff53488deee28894ff54
Tushar Seth <tusharrealme@gmail.com>
2025-10-11 21:12:09 +0530

    Updated DB Configurations

--- backend/src/main/java/com/tushar/geotrackr/GeoTrackrApplication.java
+++ backend/src/main/java/com/tushar/geotrackr/GeoTrackrApplication.java
@@ -1,5 +1,6 @@
 package com.tushar.geotrackr;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,14 @@ public class GeoTrackrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GeoTrackrApplication.class, args);
+	}
+
+	@PostConstruct
+	public void fixRenderDatabaseUrl() {
+		String dburl = System.getenv("DATABASE_URL");
+		if (dburl != null && dburl.startsWith("postgresql://")) {
+			String jdbcUrl = dburl.replace("postgresql://", "jdbc:postgresql://");
+			System.setProperty("spring.datasource.url", jdbcUrl);
+		}
 	}
 }
